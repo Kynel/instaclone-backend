f22f4f34d03c6e75dcdcd4c0f21cb3a15a29c62f
kynel <kynel95@gmail.com>
2021-10-15 15:43:20 +0900

    manage env with dotenv

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import { ApolloServer } from 'apollo-server';
+require('dotenv').config();
+import { ApolloServer, forEachField } from 'apollo-server';
+import { PossibleFragmentSpreadsRule } from 'graphql';
 import schema from './schema';
 
 const server = new ApolloServer({ schema });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log('Server is running on http://localhost:4000'));
+  .listen(PORT)
+  .then(() => console.log(`🚀 Server is running on http://localhost:${PORT}/`));
